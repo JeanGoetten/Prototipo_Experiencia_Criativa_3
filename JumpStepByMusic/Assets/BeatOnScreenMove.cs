1d59108b44c4858cedaf40e3dacabb7f00f29afd
@JeanGoetten <selbestdenkerdot@hotmail.com>
2024-03-07 08:42:24 -0300

    add 3d model for tree + fish

--- JumpStepByMusic/Assets/BeatOnScreenMove.cs
+++ JumpStepByMusic/Assets/BeatOnScreenMove.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class BeatOnScreenMove : MonoBehaviour
@@ -7,18 +6,25 @@ public class BeatOnScreenMove : MonoBehaviour
     [SerializeField] private GameObject beatOnScreen;
     private int beatCounter = 0;
     private float x = 150f;
+    [SerializeField] private float stepIncrement = 200f;
+
+    [SerializeField] private GameObject imgBeatBar;
+    [SerializeField] private Color color01;
+    [SerializeField] private Color color02;
+    private bool colorChanged;
 
     private void Start()
     {
-        //beatOnScreen.transform.position = new Vector3(x, beatOnScreen.transform.position.y, beatOnScreen.transform.position.z);
+        
+        colorChanged = false;
     }
 
     public void MoveBeatOnScreen()
     {
         beatCounter++;
-        if(beatCounter < 16)
+        if(beatCounter < 8)
         {
-            x += 106.6666666666667f; 
+            x += stepIncrement; 
             beatOnScreen.transform.position = new Vector3(x, beatOnScreen.transform.position.y, beatOnScreen.transform.position.z); 
         }
         else
@@ -26,6 +32,21 @@ public class BeatOnScreenMove : MonoBehaviour
             beatCounter = 0; 
             x = 150f; 
             beatOnScreen.transform.position = new Vector3(x, beatOnScreen.transform.position.y, beatOnScreen.transform.position.z);
+        }
+    }
+    public void ChangeColor()
+    {
+        if(colorChanged)
+        {
+            imgBeatBar.GetComponent<Image>().color = color01;
+            colorChanged = false;
+            Debug.Log("Color 1"); 
+        }
+        else
+        {
+            imgBeatBar.GetComponent<Image>().color = color02;
+            colorChanged = true;
+            Debug.Log("Color 2");
         }
     }
 }
